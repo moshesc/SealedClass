6f584d52facbffd762f9bee02de1a502e9a6c47c
moshesc <beepbeep9860@gmail.com>
2018-06-13 08:43:23 +0100

    SealdClass

---
+++ SealedClass/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SealedClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //creating the object of seald class
+            CustomerDetails obj = new CustomerDetails();
+            string Name = obj.AccountInfo();
+
+            //Writing output to console
+            Console.WriteLine(Name);
+            Console.ReadLine();
+        }
+    }
+
+    public sealed class CustomerDetails //Sealed Class
+    {
+        public string AccountInfo()
+        {
+            return "John Choan";
+        }
+    }
+}
